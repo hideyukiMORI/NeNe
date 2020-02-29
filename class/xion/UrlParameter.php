c027cae0c36951d696289c15a25fbce4785e2977
xi <info@xion.cc>
2020-02-29 21:36:26 +0900

    add : Add URI parameter class.

---
+++ class/xion/UrlParameter.php
@@ -0,0 +1,46 @@
+<?php
+namespace Nene\Xion;
+
+/**
+ * AYANE : ayane.co.jp
+ * powerd by NENE.
+ *
+ * @author hideyuki MORI
+ */
+
+/**
+ * URL Parameter class.
+ * Holds parameters passed in the form of a URL.
+ * RequestVariables class is inherited.
+ *
+ * By using this class you can get the value with a cleaner URL than GET.
+ *
+ * @author      HideyukiMORI
+ */
+class UrlParameter extends RequestVariables
+{
+    /**
+     * setValues
+     * Parse URL parameters and set it to an internal variable.
+     * Separate the URL after the controller
+     * and the action with / and interpret each part as "key_value".
+     */
+    final protected function setValues()
+    {
+        $param = rtrim($_SERVER['REQUEST_URI'], '/');
+        $params = [];
+        if ('' != $param) {
+            $params = explode('/', $param);
+        }
+        if (3 < count($params)) {
+            foreach ($params as $param) {
+                $splited = explode('_', $param);
+                if (2 == count($splited)) {
+                    $key = $splited[0];
+                    $val = $splited[1];
+                    $this->_values[$key] = $val;
+                }
+            }
+        }
+    }
+}
