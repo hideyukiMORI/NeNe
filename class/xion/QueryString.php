7871696b14cdf7cce56404bcce2218d15feb1efc
xi <info@xion.cc>
2020-02-29 21:15:17 +0900

    add : Add GET class.

---
+++ class/xion/QueryString.php
@@ -0,0 +1,34 @@
+<?php
+namespace Nene\Xion;
+
+/**
+ * AYANE : ayane.co.jp
+ * powerd by NENE.
+ *
+ * @author hideyuki MORI
+ */
+
+/**
+ * GET class
+ *
+ * Class that retains the contents of GET.
+ * RequestVariables class is inherited.
+ *
+ * This class is for backward compatibility.
+ * It is recommended to use filter_input for POST and GET.
+ *
+ * @author HideyukiMORI
+ */
+class QueryString extends RequestVariables
+{
+    /**
+     * Set value
+     * Get $_GET and set it to an internal variable.
+     */
+    protected function setValues()
+    {
+        foreach ($_GET as $key => $value) {
+            $this->_values[$key] = $value;
+        }
+    }
+}
