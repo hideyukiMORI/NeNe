40cbd41403e38992c7ec3b7f365a3f24af7b1ef1
xi <info@xion.cc>
2020-02-29 19:09:22 +0900

    add : Add site configuration file.

---
+++ ini/xSiteIni.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * AYANE : ayane.co.jp
+ * powerd by NENE.
+ *
+ * @author hideyuki MORI
+ */
+
+
+define('SITE_TITLE',    'NeNe-PHP');
+define('COPYRIGHT',  'AYANE International Inc.');
+
+
+
+
