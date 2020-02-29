3ecb3a99ed0d2cbceaf621ec7f2d043365060989
xi <info@xion.cc>
2020-02-29 19:34:22 +0900

    add : Change PHP settings.

--- ini/xSystemIni.php
+++ ini/xSystemIni.php
@@ -8,9 +8,6 @@
  */
 
 // PHP CORE SETTING
-ini_set('display_errors', 1);                                       // DISPLAY ERROR
-error_reporting(E_ALL);                                             // ERROR REPORT
-session_cache_expire(180);                                          // SESSION => 3H
 date_default_timezone_set('Asia/Tokyo');                            // TIME ZONE
 session_start();                                                    // SESSION START
 
