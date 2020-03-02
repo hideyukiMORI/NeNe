b0ad1574fdbe86f2dd220cb82d0f65c85db7ff94
xi <info@xion.cc>
2020-03-02 23:32:28 +0900

    add : Add user table data mapper.

---
+++ class/db/UserMapper.php
@@ -0,0 +1,28 @@
+<?php
+namespace Nene\Database;
+
+use Nene\Xion\DataMapperBase as DataMapperBase;
+use Nene\Database;
+use PDO;
+
+/**
+ * AYANE : ayane.co.jp
+ * powerd by NENE.
+ *
+ * @author hideyuki MORI
+ */
+
+
+/**
+ * User account data mapper.
+ */
+class UserMapper extends DataMapperBase
+{
+    const MODEL_CLASS = 'Nene\Database\User';
+    const TARGET_TABLE = 'users';
+    const KEY_SID = 'id';
+
+
+
+
+}
