cdb697ba3be33dbb8bea28d1bec17735e7ac53ce
xi <info@xion.cc>
2020-03-03 17:39:59 +0900

    add : Add view class.

---
+++ class/xion/View.php
@@ -0,0 +1,250 @@
+<?php
+namespace Nene\Xion;
+
+use Smarty;
+use PDOStatement;
+
+/**
+ * AYANE : ayane.co.jp
+ * powerd by NENE.
+ *
+ * @author hideyuki MORI
+ */
+
+/**
+ * VIEW
+ * Class to manage display.
+ */
+class View
+{
+    private static $instance;        // INSTANCE VARIABLE
+    public  $smarty;                 // SMARTY OBJECT
+    private $template;               // TEMPLATE FILE NAME
+    private $cssArray = [];          // CSS FILE NAME ARRAY
+    private $jsArray  = [];          // JAVASCRIPT FILE NAME ARRAY
+
+
+
+    /**
+     * CONSTRUCTOR.
+     */
+    final private function __construct()
+    {
+        $this->smarty = new Smarty();                           // SMARTY OBJECT
+        $this->smarty->template_dir = DIR_SMARTY_TEMPLATE;      // TEMPLATE DIR
+        $this->smarty->compile_dir  = DIR_SMARTY_COMPILE;       // TEMPLATE COMPILE DIR
+        $this->smarty->config_dir   = DIR_SMARTY_CONFIG;        // CONFIG DIR
+        $this->smarty->escape_html  = true;
+        $this->addCSS('common');
+        $this->addJS('common');
+        $this->setValue('t_contents', '');
+    }
+
+
+
+    /**
+     * Get instance.
+     * Returns the display management singleton class.
+     *
+     * @return View Singleton class for display management.
+     */
+    final public static function getInstance() : View
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+
+
+    /**
+     * Set template
+     * Set the template file.
+     *
+     * @param string $p_template  Template file name.
+     * @return void
+     */
+    final public function setTemplate(string $p_template)
+    {
+        $this->template = $p_template;
+    }
+
+
+
+    /**
+     * Set title
+     * Set the title name of the page.
+     *
+     * @param string $p_title Page title name
+     * @return void
+     */
+    final public function setTitle(string $p_title)
+    {
+        $this->smarty->assign('t_title', $p_title);
+    }
+
+
+
+    /**
+     * Add css
+     * Add the style sheet to use.
+     *
+     * @param string $p_css Style sheet file name.
+     * @return void
+     */
+    final public function addCSS(string $p_css)
+    {
+        if (strlen($p_css) > 0) {
+            $this->cssArray[] = 'css/'.$p_css;
+        }
+    }
+
+
+
+    /**
+     * Set css
+     * Set the style sheet to be used in the view.
+     *
+     * @return void
+     */
+    final public function setCSS()
+    {
+        $cssArray = [];
+        foreach ($this->cssArray as $filename) {
+            if (file_exists(DOCUMENT_ROOT.$filename.'.css')){
+                $fileTime = filemtime(DOCUMENT_ROOT . $filename.'.css');
+                $cssArray[] = array('filename' => $filename, 'filetime' => $fileTime);
+            }
+        }
+        $this->setValues('t_css', $cssArray);
+    }
+
+
+
+    /**
+     * Add javascript
+     * Add javascript to be used.
+     *
+     * @param string Javascript file name.
+     * @return void
+     */
+    final public function addJS(string $p_js)
+    {
+        if (strlen($p_js) > 0) {
+            $this->jsArray[] = 'js/'.$p_js;
+        }
+    }
+
+
+
+    /**
+     * Set javascript
+     * Set the javascript to be used in the view.
+     *
+     * @return void
+     */
+    final public function setJS()
+    {
+        $jsArray = [];
+        foreach ($this->jsArray as $filename) {
+            if (file_exists(DOCUMENT_ROOT.$filename.'.js')){
+                $fileTime = filemtime(DOCUMENT_ROOT . $filename.'.js');
+                $jsArray[] = array('filename' => $filename, 'filetime' => $fileTime);
+            }
+        }
+        $this->setValues('t_js', $jsArray);
+    }
+
+
+
+    /**
+     * Set value.
+     * Set the value in the template.
+     *
+     * @param string $p_target  Target variable name in template file.
+     * @param string $p_value   The value to set.
+     * @return void
+     */
+    final public function setValue(string $p_target, string $p_value)
+    {
+        $this->smarty->assign($p_target, $p_value);
+    }
+
+
+
+    /**
+     * Set values.
+     * Set the array in the template.
+     *
+     * @param string $p_target  Target variable name in template file.
+     * @param array $p_value    The array to set.
+     * @return void
+     */
+    final public function setValues(string $p_target, array $p_value)
+    {
+        $this->smarty->assign($p_target, $p_value);
+    }
+
+
+
+    /**
+     * Set object.
+     *
+     * @param object $p_target  Target variable name in template file.
+     * @param string $p_value   The object to set.
+     * @return void
+     */
+    final public function setObject(string $p_target, PDOStatement $p_value)
+    {
+        $this->smarty->assign($p_target, $p_value);
+    }
+
+
+
+    /**
+     * Execute Display
+     * Output page.
+     *
+     * @return void
+     */
+    final public function execute()
+    {
+        $this->setCSS();
+        $this->setJS();
+        $this->smarty->display($this->smarty->template_dir[0].''.$this->template);
+    }
+
+
+
+    /**
+     * Display error
+     * Output error page.
+     *
+     * @param  string $p_message  Error message text.
+     * @return void
+     */
+    final public function error(string $p_message)
+    {
+        $this->setTitle('ERROR');
+        $this->setValue('t_message', $p_message);
+        $this->setTemplate('error.tpl');
+        $this->setValue('t_root', URI_ROOT);
+        $this->setValue('t_controller', APP_CONTROLLER);
+        $this->setValue('t_controller_action', APP_CONTROLLER.'_'.APP_ACTION);
+        $this->execute();
+        exit;
+    }
+
+
+
+    /**
+     * Copy inhibit.
+     *
+     * @throws RuntimeException
+     */
+    final public function __clone()
+    {
+        throw new RuntimeException('Clone is not allowed against '.get_class($this));
+    }
+}
