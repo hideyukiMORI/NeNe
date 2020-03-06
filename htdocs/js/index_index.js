27a0552a22f36471c71abd76f2e9c311e2490239
xi <info@xion.cc>
2020-03-06 23:28:16 +0900

    update : Add parameter.

--- htdocs/js/index_index.js
+++ htdocs/js/index_index.js
@@ -2,10 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     window.app = new Vue({
         el: '#app',
         data: {
-            message: 'Hello NeNe!'
+            message: 'Hello NeNe!',
+            formUserId:     '',
+            formUserIdMsg:  '',
+            formUserPass:   '',
+            formUserPassMsg:'',
+            isConnect:      false,
         },
         created() {
             document.getElementById('app').classList.remove('hidden');
+        },
+        methods: {
+            onSubmit: function() {
+                console.log('submit!!');
+            }
         }
     });
 });
