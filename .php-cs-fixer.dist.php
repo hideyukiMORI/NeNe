df79cb72a373f6a0cdff7a811c997ce1934a3401
hideyukiMORI <info@ayane.co.jp>
2022-01-22 01:21:40 +0900

    add : Added phpcs settings

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This document has been generated with
+ * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.1.0|configurator
+ * you can change this configuration by importing this file.
+ */
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true, // ここにルール追記
+        'declare_strict_types' => true
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+        ->exclude([ // 除外ファイル
+              'vendor'
+        ])
+        ->in(__DIR__)
+    )
+;
