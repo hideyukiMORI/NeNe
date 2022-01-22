7da9105e43850f711093c60fede46d763c9fd279
hideyukiMORI <info@ayane.co.jp>
2022-01-23 00:27:44 +0900

    update : Write php doc

--- class/xion/DataModelBase.php
+++ class/xion/DataModelBase.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * AYANE : ayane.co.jp
  * powered by NENE.
@@ -14,6 +12,8 @@ declare(strict_types=1);
  * @license   https://choosealicense.com/no-permission/ NO LICENSE
  * @link      https://ayane.co.jp/
  */
+
+declare(strict_types=1);
 
 namespace Nene\Xion;
 
@@ -40,10 +40,40 @@ abstract class DataModelBase
     protected const STRING        = 'string';
     protected const DATETIME      = 'dateTime';
     protected const DATE          = 'date';
+
+    /**
+     * Row data.
+     *
+     * @var array
+     */
     protected $data    = [];
+
+    /**
+     * Table schema
+     *
+     * @var array
+     */
     protected static $schema  = [];
+
+    /**
+     * Logger
+     *
+     * @var Log
+     */
     protected $LOGGER;
+
+    /**
+     * Class name
+     *
+     * @var string
+     */
     protected $CLASS;
+
+    /**
+     * Error code
+     *
+     * @var ErrorCode
+     */
     protected $ERROR_CODE;
 
     /**
@@ -60,7 +90,14 @@ abstract class DataModelBase
         $this->ERROR_CODE = Xion\ErrorCode::getInstance();
     }
 
-    public function __get($prop)
+    /**
+     * getter
+     *
+     * @param string $prop Parameter key.
+     *
+     * @return string|null
+     */
+    public function __get(string $prop)
     {
         if (isset($this->data[$prop])) {
             return $this->data[$prop];
@@ -73,12 +110,27 @@ abstract class DataModelBase
         }
     }
 
-    public function __isset($prop): bool
+    /**
+     * Is set param.
+     *
+     * @param string $prop Parameter key.
+     *
+     * @return boolean
+     */
+    public function __isset(string $prop): bool
     {
         return isset($this->data[$prop]);
     }
 
-    public function set($prop, $val): bool
+    /**
+     * setter
+     *
+     * @param string $prop Parameter key.
+     * @param mixed  $val  Parameter value.
+     *
+     * @return boolean
+     */
+    public function set(string $prop, mixed $val): bool
     {
         if (!$this->validate($prop, $val)) {
             return false;
@@ -87,7 +139,16 @@ abstract class DataModelBase
         return true;
     }
 
-    public function setParam($prop, $val)
+    /**
+     * Set the value for the parameter.
+     * It corresponds to the method chain.
+     *
+     * @param string $prop Parameter key.
+     * @param mixed  $val  Parameter value.
+     *
+     * @return DataModelBale
+     */
+    public function setParam(string $prop, mixed $val)
     {
         if (!$this->validate($prop, $val)) {
             throw new \Exception('Parameter ' . $prop . ' is a validation error. (value : ' . $val . ')');
@@ -101,12 +162,12 @@ abstract class DataModelBase
      *
      * Set the POST data obtained from filter_input to the specified parameter.
      *
-     * @param string $prop  Object parameter name.
-     * @param string $postProp  POST parameter name.
+     * @param string $prop     Object parameter name.
+     * @param string $postProp POST parameter name.
      *
      * @return DataModelBase
      */
-    public function setParamPostString($prop, $postProp = '')
+    public function setParamPostString(string $prop, string $postProp = '')
     {
         $postProp = $postProp == '' ? $prop : $postProp;
         return $this->setParam($prop, (string)filter_input(INPUT_POST, $postProp));
@@ -117,18 +178,27 @@ abstract class DataModelBase
      *
      * Set the POST data obtained from filter_input to the specified parameter.
      *
-     * @param string $prop  Object parameter name.
-     * @param string $postProp  POST parameter name.
+     * @param string $prop     Object parameter name.
+     * @param string $postProp POST parameter name.
      *
      * @return DataModelBase
      */
-    public function setParamPostInt($prop, $postProp = '')
+    public function setParamPostInt(string $prop, string $postProp = '')
     {
         $postProp = $postProp == '' ? $prop : $postProp;
         return $this->setParam($prop, (int)filter_input(INPUT_POST, $postProp));
     }
 
-    public function __set($prop, $val)
+    /**
+     * Setter.
+     * The value is set in the parameter according to the type set in the schema.
+     *
+     * @param string $prop Parameter key.
+     * @param mixed  $val  Parameter value.
+     *
+     * @return mixed
+     */
+    public function __set(string $prop, mixed $val)
     {
         if (!isset(static::$schema[$prop])) {
             echo 'DATA MODEL ERROR. Unable to set parameters. The property "' . $prop . '" may not be defined.';
@@ -140,7 +210,7 @@ abstract class DataModelBase
         $type = gettype($val);
         if ($type === $schema) {
             $this->data[$prop] = $val;
-            return;
+            return true;
         }
 
         switch ($schema) {
@@ -156,18 +226,35 @@ abstract class DataModelBase
         }
     }
 
+    /**
+     * Returns data as an array type.
+     *
+     * @return array
+     */
     public function toArray(): array
     {
         return $this->data;
     }
 
-    public function fromArray(array $arr)
+    /**
+     * Set the value of the parameter from the array.
+     *
+     * @param array $arr An associative array with the values ​​you want to set.
+     *
+     * @return void
+     */
+    public function fromArray(array $arr): void
     {
         foreach ($arr as $key => $val) {
             $this->__set($key, $val);
         }
     }
 
+    /**
+     * Check if the value is valid
+     *
+     * @return boolean
+     */
     abstract public function isValid();
 
     /**
@@ -187,11 +274,12 @@ abstract class DataModelBase
      *
      * Validate parameters.
      *
-     * @param mixed $param The value to validate.
+     * @param mixed $param         The value to validate.
      * @param array $validateArray An array of validation conditions.
-     * @return bool Validation results.
+     *
+     * @return boolean Validation results.
      */
-    protected function doValid($param, $validateArray)
+    protected function doValid(mixed $param, array $validateArray): bool
     {
         $flag = true;
         foreach ($validateArray as $key => $value) {
@@ -251,17 +339,23 @@ abstract class DataModelBase
      * Use this method when the creation date and update date of the database row are string type for some reason.
      * If the creation stamp has not been set, set the creation stamp in the format of "HmdHi".
      * And set the update stamp in the format of "HmdHi".
+     *
+     * @return void
      */
-    public function setNow()
+    public function setNow(): void
     {
         if (strlen($this->created_at) <= 1) {
             $this->created_at = date('YmdHi');
         }
         $this->updated_at = date('YmdHi');
-        return;
     }
 
-    public function getSchema()
+    /**
+     * Get schema.
+     *
+     * @return array
+     */
+    public function getSchema(): array
     {
         return static::$schema;
     }
