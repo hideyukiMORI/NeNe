8b1533ce0a5197fbfc7aba2abba0fe3e91f0aff6
hideyukiMORI <info@ayane.co.jp>
2022-01-23 22:42:37 +0900

    update : Write php doc

--- class/func/Json.php
+++ class/func/Json.php
@@ -13,6 +13,8 @@
  * @link      https://ayane.co.jp/
  */
 
+declare(strict_types=1);
+
 namespace Nene\Func;
 
 /**
@@ -36,29 +38,31 @@ class Json
     {
         $postJson   = file_get_contents('php://input');
         $jsonArray  = json_decode($postJson, true);
-        $jsonArray  = is_array($jsonArray) ? $jsonArray : array();
-        return $jsonArray;
+        return is_array($jsonArray) ? $jsonArray : [];
     }
 
     /**
      * Output Array to Json.
      * Output the argument that was casted array to json.
      *
-     * @param array $jsonArray Array that you want to convert to JSON format.
+     * @param array   $jsonArray Array that you want to convert to JSON format.
+     * @param string  $format    Specified format.
+     * @param string  $callback  Callback function name.
+     * @param boolean $session   Whether authentication is required.
      *
      * @return void
      */
     final public static function outputArrayToJson(
         array $jsonArray,
-        string $style = 'jsonp',
+        string $format = 'jsonp',
         string $callback = 'jsonCallback',
         bool $session = false
-    ) {
+    ): void {
         $responseArray = [
             'Result' => true,
             'Data'   => $jsonArray
         ];
-        if ($style == 'jsonp') {
+        if ($format == 'jsonp') {
             if ($callback == null || !$callback) {
                 $callback = 'jsonCallback';
             }
@@ -76,12 +80,12 @@ class Json
     /**
      * Output Error in JSON format.
      *
-     * @param string $errorCode     Error code.
-     * @param string $errorMessage  Error message.
+     * @param string $errorCode    Error code.
+     * @param string $errorMessage Error message.
      *
      * @return void
      */
-    final public static function outputErrorInJson(string $errorCode, string $errorMessage)
+    final public static function outputErrorInJson(string $errorCode, string $errorMessage): void
     {
         $responseArray = [
             'Result' => false,
